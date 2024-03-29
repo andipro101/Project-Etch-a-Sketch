d9e64f3c53a466566ebed3da24a74e46b9ff0ab3
andipro101 <andi.parvanov@gmail.com>
2024-03-30 01:44:30 +0200

    add custom grid size

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-const gridContainer = document.querySelector("#gridContainer");
+const GRID_CONTAINER = document.querySelector("#gridContainer");
+const GRID_SIZE = document.querySelector("#gridSize")
+const DIV_SQUARE = document.createElement("div");
+DIV_SQUARE.classList.add("square");
 
-const divSquare = document.createElement("div");
-divSquare.classList.add("square");
-
-
+window.addEventListener('load', () => {
+    grid(16);
+});
 
 function changeColorPermanent(event) {
     event.target.style.backgroundColor = 'red';
     event.target.removeEventListener('mouseenter', changeColorPermanent);
 }
 
-
-
-
-
-function grid(number){
+function grid(number){    
+    
     for (let line = 0; line < number; line++) {
         for (let row = 0; row < number; row++){
-            let divSquareClone = divSquare.cloneNode(true);
+            let divSquareClone = DIV_SQUARE.cloneNode(true);
             divSquareClone.addEventListener('mouseenter', changeColorPermanent);
-            gridContainer.appendChild(divSquareClone);
+            GRID_CONTAINER.appendChild(divSquareClone);
         }
     }
 }
 
-grid(16)
 
+GRID_SIZE.addEventListener("click", () => {
+    let gridSizeInput;
+    do {
+        gridSizeInput = prompt("Please enter a grid number between 1 and 100:");
+    } while ((gridSizeInput !== null) && (isNaN(gridSizeInput) || parseInt(gridSizeInput) < 1 || parseInt(gridSizeInput) > 100));
+
+    if (gridSizeInput !== null) {
+        gridSizeInput = parseInt(gridSizeInput);
+        GRID_CONTAINER.innerHTML = "";
+        grid(gridSizeInput)
+    }
+});
