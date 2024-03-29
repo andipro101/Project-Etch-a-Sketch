dfa012b094dee60fa18756d240c4b75a3d8cb7c8
andipro101 <andi.parvanov@gmail.com>
2024-03-29 23:37:01 +0200

    add mousehover backgroundcolor change, make container bigger

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ const divSquare = document.createElement("div");
 divSquare.classList.add("square");
 
 
-for (let line = 0; line < 16; line++) {
-    for (let row = 0; row < 16; row++){
-        let divSquareClone = divSquare.cloneNode(true);
-        gridContainer.appendChild(divSquareClone);
+
+function changeColorPermanent(event) {
+    event.target.style.backgroundColor = 'red';
+    event.target.removeEventListener('mouseenter', changeColorPermanent);
+}
+
+
+
+
+
+function grid(number){
+    for (let line = 0; line < number; line++) {
+        for (let row = 0; row < number; row++){
+            let divSquareClone = divSquare.cloneNode(true);
+            divSquareClone.addEventListener('mouseenter', changeColorPermanent);
+            gridContainer.appendChild(divSquareClone);
+        }
     }
 }
+
+grid(16)
+
