c835f6deb6b0a1f5c040d00b7dbfc6777521a55f
andipro101 <andi.parvanov@gmail.com>
2024-03-30 15:32:30 +0200

    change grid size to be selected by slider

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const GRID_CONTAINER = document.querySelector("#gridContainer");
-const GRID_SIZE = document.querySelector("#gridSize")
-const DIV_SQUARE = document.createElement("div");
+const GRID_SIZE_BUTTON = document.querySelector("#gridSize")
+const RANGE_SLIDER = document.querySelector("#rangeSlider")
+const RANGE_OUTPUT = document.querySelector("#sliderValue")
 
 window.addEventListener('load', () => {
-    grid(16);
+    // grid(16);
+    RANGE_OUTPUT.innerHTML = RANGE_SLIDER.value;
+    grid(RANGE_SLIDER.value);
 });
+
+
+
+
+function handleSliderChange() {
+    RANGE_OUTPUT.innerHTML = RANGE_SLIDER.value;
+    GRID_CONTAINER.innerHTML = "";
+    // Call grid function with new value
+    grid(RANGE_SLIDER.value);
+}
+
+RANGE_SLIDER.addEventListener("input", handleSliderChange)
+
 
 function changeColorPermanent(event) {
     event.target.style.backgroundColor = 'red';
@@ -12,7 +28,9 @@ function changeColorPermanent(event) {
 }
 
 
+
 function grid(number){    
+    const DIV_SQUARE = document.createElement("div");
     DIV_SQUARE.classList.add("square");
 
     let size = 880 / number + "px"
@@ -29,15 +47,19 @@ function grid(number){
 }
 
 
-GRID_SIZE.addEventListener("click", () => {
-    let gridSizeInput;
-    do {
-        gridSizeInput = prompt("Please enter a grid number between 1 and 100:");
-    } while ((gridSizeInput !== null) && (isNaN(gridSizeInput) || parseInt(gridSizeInput) < 1 || parseInt(gridSizeInput) > 100));
+// GRID_SIZE_BUTTON.addEventListener("click", () => {
+//     let gridSizeInput;
+//     do {
+//         gridSizeInput = prompt("Please enter a grid number between 1 and 100:");
+//     } while ((gridSizeInput !== null) && (isNaN(gridSizeInput) || parseInt(gridSizeInput) < 1 || parseInt(gridSizeInput) > 100));
 
-    if (gridSizeInput !== null) {
-        gridSizeInput = parseInt(gridSizeInput);
-        GRID_CONTAINER.innerHTML = "";
-        grid(gridSizeInput)
-    }
-});
+    
+//     if (gridSizeInput !== null) {
+//         if (gridSizeInput === ""){
+//             gridSizeInput = 16
+//         }
+//         gridSizeInput = parseInt(gridSizeInput);
+//         GRID_CONTAINER.innerHTML = "";
+//         grid(gridSizeInput)
+//     }
+// });
