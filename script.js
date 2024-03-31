c2929b96e400fd873b893fa936e162a50087eb20
andipro101 <andi.parvanov@gmail.com>
2024-03-31 05:25:17 +0300

    add diffrent colors

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const GRID_CONTAINER = document.querySelector("#gridContainer");
-const GRID_BORDER_BUTTON = document.querySelector("#gridBorder")
-const RANGE_SLIDER = document.querySelector("#rangeSlider")
-const RANGE_OUTPUT = document.querySelector("#sliderValue")
+const GRID_BORDER_BUTTON = document.querySelector("#gridBorder");
+const RANGE_SLIDER = document.querySelector("#rangeSlider");
+const RANGE_OUTPUT = document.querySelector("#sliderValue");
+const RESET_BUTTON = document.querySelector("#reset");
+const CONTAINERS = document.querySelectorAll(".colorsContainer");
 
 let isBorderOn = false;
+let currentColor = 'black';
 
 window.addEventListener('load', () => {
     RANGE_OUTPUT.innerHTML = RANGE_SLIDER.value + "x" + RANGE_SLIDER.value;
@@ -18,8 +21,11 @@ function handleSliderChange() {
 
 function toggleBorder() {
     isBorderOn = !isBorderOn;
+    updateBorder();
+}
+
+function updateBorder() {
     const squares = document.querySelectorAll('.square');
-    
     squares.forEach(square => {
         if (isBorderOn) {
             square.style.border = "1px solid black";
@@ -30,8 +36,8 @@ function toggleBorder() {
 }
 
 function changeColorPermanent(event) {
-    event.target.style.backgroundColor = 'red';
-    event.target.removeEventListener('mouseenter', changeColorPermanent);
+    event.target.style.backgroundColor = currentColor;
+
 }
 
 function grid(number){    
@@ -48,7 +54,35 @@ function grid(number){
             GRID_CONTAINER.appendChild(divSquareClone);
         }
     }
+    updateBorder();
 }
 
-RANGE_SLIDER.addEventListener("input", handleSliderChange)
-GRID_BORDER_BUTTON.addEventListener("click", toggleBorder)
+RESET_BUTTON.addEventListener("click",()=>{
+    GRID_CONTAINER.innerHTML = "";
+    grid(RANGE_SLIDER.value);
+});
+
+GRID_BORDER_BUTTON.addEventListener("click", toggleBorder);
+RANGE_SLIDER.addEventListener("input", handleSliderChange);
+
+CONTAINERS.forEach(container => {
+    container.addEventListener('click', (event) => {
+        if (event.target.classList.contains('colorButton')) {
+            if (event.target.id === 'random') {
+                currentColor = getRandomColor();
+            } else {
+                currentColor = event.target.id;
+            }
+        }
+    });
+});
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
